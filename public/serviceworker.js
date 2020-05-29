e44872502c7123c71c9a8390ef2d56a76d3b0d5f
anant-bahuguna <anantbahugunaddn@gmail.com>
2020-05-30 01:27:27 +0530

    Complete PWA

---
+++ public/serviceworker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html", "offline.html"];
+
+const self = this;
+
+// INSTALL SW
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log("Opened cache");
+
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Listen for requests
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        caches.match(e.request).then(() => {
+            return fetch(e.request).catch(() => caches.match("offline.html"));
+        })
+    );
+});
+// Activate SW
+self.addEventListener("activate", (e) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    e.waitUntil(
+        caches.keys().then((cacheNames) =>
+            Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            )
+        )
+    );
+});
